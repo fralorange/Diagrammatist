5a33714190392443e5301f24aa1820cc16620f43
fralorange <fralorange@mail.ru>
2024-03-15 22:12:57 +0300

    Initial commit: Create .NET MAUI Project 1. Remove Windows Title Bar from .NET MAUI project 2. Clear MainView.xaml and MainView.xaml.cs 3. Create specific folder for Views.

---
+++ src/Presentation/DiagramApp.Client/MauiProgram.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.LifecycleEvents;
+
+namespace DiagramApp.Client
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if WINDOWS
+            builder.ConfigureLifecycleEvents(events =>
+            {
+                events.AddWindows(windowsLifecycleBuilder =>
+                {
+                    windowsLifecycleBuilder.OnWindowCreated(window =>
+                    {
+                        window.ExtendsContentIntoTitleBar = false;
+                        var handle = WinRT.Interop.WindowNative.GetWindowHandle(window);
+                        var id = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(handle);
+                        var appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(id);
+                        switch (appWindow.Presenter)
+                        {
+                            case Microsoft.UI.Windowing.OverlappedPresenter overlappedPresenter:
+                                overlappedPresenter.SetBorderAndTitleBar(false, false);
+                                overlappedPresenter.IsMaximizable = false;
+                                overlappedPresenter.IsResizable = false;
+                                overlappedPresenter.IsMinimizable = false;
+                                break;
+                        }
+                    });
+                });
+            });
+#endif
+
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
---
+++ src/Presentation/DiagramApp.Client/Views/MainView.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace DiagramApp.Client
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+}
