61db134fa8e4586a624159c686ae274d5b154007
fralorange <fralorange@mail.ru>
2024-04-06 19:04:25 +0300

    Feature-6: 1. Add default option for background type in creating canvas menu 2. Add checkbox to alter object themes

--- src/Domain/DiagramApp.Domain/DiagramSettings/DiagramSettings.cs
+++ src/Domain/DiagramApp.Domain/DiagramSettings/DiagramSettings.cs
@@ -5,7 +5,7 @@
         public string FileName { get; set; } = string.Empty;
         public int Width { get; set; } = 512;
         public int Height { get; set; } = 512;
-        public BackgroundType Background { get; set; } = BackgroundType.White;
+        public BackgroundType Background { get; set; } = BackgroundType.Default;
         public DiagramType Type { get; set; } = DiagramType.Custom;
     }
 }
