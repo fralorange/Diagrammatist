8497cfe863add0c320e940c2e0aae327d3d17555
fralorange <fralorange@mail.ru>
2024-04-22 20:10:02 +0300

    Bug fixes: Major. After undoing object from canvas and redoing it again properties doesn't change Minor. Add Fallback values for CanUndo and CanRedo variables

--- src/Presentation/DiagramApp.Client/Components/CommonToolboxView.xaml.cs
+++ src/Presentation/DiagramApp.Client/Components/CommonToolboxView.xaml.cs
@@ -13,7 +13,7 @@ public partial class CommonToolboxView : Grid
         InitializeComponent();
     }
 
-    private void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+    private async void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
     {
         if (sender is CollectionView { SelectedItem: { } } collectionView)
         {
@@ -23,40 +23,38 @@ public partial class CommonToolboxView : Grid
                 var figure = current!.Figure;
 
                 ObservableFigure? observableFigure = null;
-                Action addAction = async () =>
+                switch (figure)
                 {
-                    switch (figure)
-                    {
-                        case PathFigure pathFigure:
-                            observableFigure = new ObservablePathFigure(new PathFigure { Name = pathFigure.Name, PathData = pathFigure.PathData });
-                            break;
-                        case PolylineFigure polylineFigure:
-                            var blockedPoints = await viewModel.CurrentCanvas.BlockAsync<List<System.Drawing.Point>>();
-                            if (blockedPoints is null) break;
-                            var points = new List<System.Drawing.Point>(blockedPoints);
-                            var (translatedX, translatedY) = points.Count > 1
-                                ? (points.Min(pt => pt.X), points.Min(pt => pt.Y))
-                                : (default(int), default(int));
-                            NormalizePoints(points, (translatedX, translatedY));
-                            observableFigure = new ObservablePolylineFigure(new PolylineFigure
-                            {
-                                Name = polylineFigure.Name,
-                                Points = points.Count > 1 ? points : polylineFigure.Points
-                            })
-                            {
-                                TranslationX = points.Count > 1 ? translatedX : default,
-                                TranslationY = points.Count > 1 ? translatedY : default
-                            };
-                            break;
-                        case TextFigure textFigure:
-                            observableFigure = new ObservableTextFigure(new TextFigure { Name = textFigure.Name, Text = textFigure.Text });
-                            break;
-                    }
+                    case PathFigure pathFigure:
+                        observableFigure = new ObservablePathFigure(new PathFigure { Name = pathFigure.Name, PathData = pathFigure.PathData });
+                        break;
+                    case PolylineFigure polylineFigure:
+                        var blockedPoints = await viewModel.CurrentCanvas.BlockAsync<List<System.Drawing.Point>>();
+                        if (blockedPoints is null) break;
+                        var points = new List<System.Drawing.Point>(blockedPoints);
+                        var (translatedX, translatedY) = points.Count > 1
+                            ? (points.Min(pt => pt.X), points.Min(pt => pt.Y))
+                            : (default(int), default(int));
+                        NormalizePoints(points, (translatedX, translatedY));
+                        observableFigure = new ObservablePolylineFigure(new PolylineFigure
+                        {
+                            Name = polylineFigure.Name,
+                            Points = points.Count > 1 ? points : polylineFigure.Points
+                        })
+                        {
+                            TranslationX = points.Count > 1 ? translatedX : default,
+                            TranslationY = points.Count > 1 ? translatedY : default
+                        };
+                        break;
+                    case TextFigure textFigure:
+                        observableFigure = new ObservableTextFigure(new TextFigure { Name = textFigure.Name, Text = textFigure.Text });
+                        break;
+                }
 
-                    viewModel.CurrentCanvas.Figures.Add(observableFigure!);
-                };
+                var action = new Action(() => viewModel.CurrentCanvas.Figures.Add(observableFigure!));
+                var undoAction = new Action(() => viewModel.CurrentCanvas.Figures.Remove(observableFigure!));
 
-                UndoableCommandHelper.ExecuteAction(viewModel.CurrentCanvas, addAction, () => viewModel.CurrentCanvas.Figures.Remove(observableFigure!));
+                UndoableCommandHelper.ExecuteAction(viewModel.CurrentCanvas, action, undoAction);
             }
 
             Dispatcher.Dispatch(() =>
