5fc8b7cd4c5524ea1bdad0f967807c0d5a3ce6d1
fralorange <fralorange@mail.ru>
2025-05-04 03:54:02 +0300

    Implement new styles for tabs components and drag'n'drop functionality

--- src/Presentation/Diagrammatist.Presentation.WPF/Views/Components/TabsComponent.xaml.cs
+++ src/Presentation/Diagrammatist.Presentation.WPF/Views/Components/TabsComponent.xaml.cs
@@ -1,10 +1,15 @@
-﻿using Diagrammatist.Presentation.WPF.Core.Helpers;
+﻿using Diagrammatist.Presentation.WPF.Core.Foundation.Extensions;
+using Diagrammatist.Presentation.WPF.Core.Helpers;
+using Diagrammatist.Presentation.WPF.Core.Models.Canvas;
 using Diagrammatist.Presentation.WPF.Core.Services.Alert;
 using Diagrammatist.Presentation.WPF.Core.Shared.Enums;
 using Diagrammatist.Presentation.WPF.ViewModels.Components;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Win32;
+using System.Collections.ObjectModel;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace Diagrammatist.Presentation.WPF.Views.Components
 {
@@ -18,6 +23,9 @@ namespace Diagrammatist.Presentation.WPF.Views.Components
     {
         private readonly IAlertService _alertService;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TabsComponent"/> class.
+        /// </summary>
         public TabsComponent()
         {
             var viewModel = App.Current.Services.GetRequiredService<TabsViewModel>();
@@ -75,5 +83,59 @@ namespace Diagrammatist.Presentation.WPF.Views.Components
 
             return string.Empty;
         }
+
+        #region UI Event handlers
+
+        private void TabsListBox_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed && sender is ListBox listBox)
+            {
+                var point = e.GetPosition(listBox);
+                var item = listBox.InputHitTest(point) as DependencyObject;
+
+                var container = item?.GetVisualAncestor<ListBoxItem>();
+                if (container != null && listBox.ItemContainerGenerator.ItemFromContainer(container) is CanvasModel data)
+                {
+                    DragDrop.DoDragDrop(container, data, DragDropEffects.Move);
+                }
+            }
+        }
+
+        private void TabsListBox_DragOver(object sender, DragEventArgs e)
+        {
+            e.Effects = e.Data.GetDataPresent(typeof(CanvasModel)) ? DragDropEffects.Move : DragDropEffects.None;
+            e.Handled = true;
+        }
+
+        private void TabsListBox_Drop(object sender, DragEventArgs e)
+        {
+            if (!e.Data.GetDataPresent(typeof(CanvasModel))) return;
+
+            var droppedData = (CanvasModel)e.Data.GetData(typeof(CanvasModel))!;
+            if (sender is not ListBox listBox) return;
+
+            var point = e.GetPosition(listBox);
+            var element = listBox.InputHitTest(point) as DependencyObject;
+            var container = element?.GetVisualAncestor<ListBoxItem>();
+
+            if (container == null) return;
+
+            var targetData = (CanvasModel)listBox.ItemContainerGenerator.ItemFromContainer(container)!;
+            if (droppedData == targetData) return;
+
+            if (listBox.ItemsSource is ObservableCollection<CanvasModel> items)
+            {
+                int oldIndex = items.IndexOf(droppedData);
+                int newIndex = items.IndexOf(targetData);
+
+                if (oldIndex >= 0 && newIndex >= 0)
+                {
+                    items.Move(oldIndex, newIndex);
+                    listBox.SelectedItem = droppedData;
+                }
+            }
+        }
+
+        #endregion
     }
 }
